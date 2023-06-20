40e3f59bc259b2eb09d3cadfa74ce1a3392040b1
intantunti15 <127914172+intantunti15@users.noreply.github.com>
2023-06-20 20:55:26 +0800

    Add files via upload

---
+++ deskripsi/deskripsi.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <div class="header">
+        <h1>DEFINISI DATABASE</h1>
+        <img src="Screenshot (138).png" width="500"height="550">
+
+        <h4>Penjelasan skema database:</h4>
+
+    <h4> 1.Tabel users: Tabel ini akan menyimpan informasi pengguna.</h4>
+    <h4> 2.id: Kolom ini adalah kolom unik yang berfungsi sebagai identitas unik untuk setiap pengguna.</h4>
+    <h4> 3.first_name: Kolom ini akan menyimpan nama pertama pengguna.</h4>
+    <h4> 4.last_name: Kolom ini akan menyimpan nama terakhir pengguna.</h4>
+
+    <h1> IDE PROJEK</h1>
+    <h4>
+        Ide projek dari website ini untuk menciptakan sebuah website, dengan fitur menampilkan berbagai jenis sepatu
+    </h4>
+    <h4>
+        tujuan utama dari projek ini adalah menyediakan website yang mudah digunakan untuk melihat berbagai jenis sepatu
+    </h4>
+
+    <h1> DESKRIPSI PROJEK</h1>
+    <h4> Nama Projek : Website Sepatu </h4>
+    <h4> Deskripsi Singkat : Proyek ini bertujuan untuk membuat sebuah website yang berfokus pada penjualan sepatu secara online. Website ini akan menyediakan berbagai macam sepatu, mulai dari sepatu olahraga, sepatu kasual, hingga sepatu formal. Pengguna dapat melihat daftar sepatu yang tersedia, melihat detail produk, harga,dan stok</h4>
+    <h4> Fitur yang tersedia</h4>
+    <h4> 1.Beranda (Home): Menampilkan gambar-gambar menarik sepatu terbaru, penawaran khusus, dan pengenalan tentang brand sepatu yang dijual</h4>
+    <h4> 2.Kategori Sepatu: Menampilkan kategori-kategori sepatu yang tersedia, seperti olahraga, kasual, formal, dan lain-lain.</h4>
+    <h4> 3.Daftar Produk: Menampilkan daftar sepatu yang tersedia beserta gambar, harga, dan deskripsi singkat. Pengguna dapat melihat detail produk dengan mengklik salah satu sepatu.</h4>
+    <h4> 4.Detail Produk: Menampilkan informasi lengkap tentang sebuah sepatu, termasuk gambar yang lebih besar, ukuran yang tersedia, fitur produk, dan ulasan pengguna.</h4>
+
+ <h3> Bahasa pemograman yang digunakan : </h3>
+ <h4> 1.HTML :  
+HTML (HyperText Markup Language): Digunakan untuk membangun struktur dan tampilan dasar halaman web, termasuk elemen formulir dan elemen lainnya.</h4>
+<h4> 2.PHP :
+PHP (Hypertext Preprocessor): Digunakan untuk mengelola permintaan dari frontend, berinteraksi dengan database, dan melakukan operasi pemrosesan server-side.</h4>
+<h4> 3. SQL :
+    Untuk menampilkan database.</h4>  
+
+ 
+
+</body>
+</html>
